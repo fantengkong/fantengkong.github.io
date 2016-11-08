7425e19f8dfe2bfd903c913b6976ec8e16165191
1228318390@qq.com <1228318390@qq.com>
2016-11-08 19:08:50 +0800

    kong

---
+++ js/views/复制 activity-controller.js
@@ -0,0 +1,87 @@
+starterCtrls
+  .controller('ActivityController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+    /*活动页数据*/
+    $scope.pastActivities = [];
+    $scope.latestActivities = []; 
+		/*获取数据*/
+    $http.get('mock/activity.json')
+			.then(
+				function(res) {
+					$scope.activities = res.data;
+					$scope.activitiesLength = $scope.activities.length;
+					for(var i=0;i<$scope.activitiesLength;i++){
+						if($scope.activities[i].info == "已结束"){
+							$scope.pastActivities.push($scope.activities[i]);
+						}else{
+							$scope.latestActivities.push($scope.activities[i]);							
+						}
+					}
+				}
+			);
+			
+		/*$scope.latestGoTo = function(index){
+			if($scope.latestActivities[index].info=="直播中"){
+				$state.go("live");
+			}else{
+				$state.go("detail", {detailId: index,view: 'activity'});
+			}
+			$state.go("detail", {detailId: index,view: 'activity'});
+		}
+		
+		$scope.pastGoTo = function(index){
+			$state.go("detail", {detailId: index,view: 'activity'});
+		}*/
+		
+  }])
+	/*$scope.doRefresh = function () {
+    $http.get('./mock/employee-more.json')
+      .then(
+        function (res) {
+          $scope.employees = res.data.concat($scope.employees);
+        }
+      )
+      .finally(function () {
+        $scope.$broadcast('scroll.refreshComplete');
+      })
+  };*/
+  .controller('lastestActCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+ 		/*活动页数据*/
+    $scope.pastActivities = [];
+    $scope.latestActivities = []; 
+		/*获取数据*/
+    $http.get('mock/activity.json')
+			.then(
+				function(res) {
+					$scope.activities = res.data;
+					$scope.activitiesLength = $scope.activities.length;
+					for(var i=0;i<$scope.activitiesLength;i++){
+						if($scope.activities[i].info == "已结束"){
+							$scope.pastActivities.push($scope.activities[i]);
+						}else{
+							$scope.latestActivities.push($scope.activities[i]);							
+						}
+					}
+				}
+			);
+ 	}])
+  .controller('pastActCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+ 		/*活动页数据*/
+    $scope.pastActivities = [];
+    $scope.latestActivities = []; 
+		/*获取数据*/
+    $http.get('mock/activity.json')
+			.then(
+				function(res) {
+					$scope.activities = res.data;
+					$scope.activitiesLength = $scope.activities.length;
+					for(var i=0;i<$scope.activitiesLength;i++){
+						if($scope.activities[i].info == "已结束"){
+							$scope.pastActivities.push($scope.activities[i]);
+						}else{
+							$scope.latestActivities.push($scope.activities[i]);							
+						}
+					}
+				}
+			);
+ 	}])
+  
